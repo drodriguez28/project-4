891114619fed84d57524920eb59d49b1844e0e60
Deivid Rodriguez <rodriguezdeivid17@gmail.com>
2019-11-25 13:59:28 -0500

    fixing

--- controllers/template.js
+++ controllers/template.js
@@ -1,50 +1,52 @@
-/* Step 1 import express
- *
- */
+
 const express = require('express')
 
-/* Step 2
- *
- * Import the api files from the models
- *
- * TODO: change the file path to the models file you'll need to use.
- * TODO: rename this from `templateApi` to something more sensible (e.g:
- * `shopsAPI`)
- *
- * NOTE: You may need to import more than one API to create the 
- * controller you need.
- * 
- */
-const templateApi = require('../models/template.js')
 
-/* Step 3 
- * 
- * Create a new router.
- *
- * the router will "contain" all the request handlers that you define in this file.
- * TODO: rename this from templateRouter to something that makes sense. (e.g:
- * `shopRouter`)
- */
-const templateRouter = express.Router()
+const fanApi = require('../models/fanModel.js')
 
-/* Step 4
- * 
- * TODO: Put all request handlers here
- */
 
-/* Step 5
- *
- * TODO: delete this handler; it's just a sample
- */ 
-templateRouter.get('/', (req, res) => {
-  res.json(templateApi.getHelloWorldString())
+const fanRouter = express.Router()
+
+//get all 
+
+fanRouter.get('/', (req, res) => {
+  fanApi.getAllFans()
+    .then((allFans) => {
+      console.log(allFans)
+      res.json(allFans)
+
+    })
 })
 
-/* Step 6
- *
- * Export the router from the file.
- *
- */
+// get one
+fanRouter.get('/:id', (req, res) => {
+  fanApi.getSingleFan(req.params.id)
+    .then((singleFan) => {
+      res.json(singleFan)
+    })
+})
+
+
+// create fan 
+fanRouter.post('/', (req, res) => {
+  fanApi.createFan(req.body)
+    .then((createdFan) => {
+      res.json(createdFan)
+    })
+})
+
+
+//delete fan
+fanRouter.delete('/:id', (req, res) => {
+  fanApi.deleteFan(req.params.id)
+    .then((deletedFan) => {
+      res.json(deletedFan)
+    })
+})
+
+
+
+
 module.exports = {
-  templateRouter
+  fanRouter
 }
