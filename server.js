3b5064c3491ebaa7dcfaaa7d717711f26919c50a
Deivid Rodriguez <rodriguezdeivid17@gmail.com>
2019-11-25 14:03:27 -0500

    added connectojn

--- server.js
+++ server.js
@@ -1,32 +1,12 @@
-/* 
- * This is the template for a server.js file.  Follow the steps below and read
- * the comments for creating your own (or you can just copy this file).
- */
 
-/* Step 1
- *
- * Import needed packages
- *
- */
 const express = require('express')
 const app = express()
 
-/* Step 2
- * 
- * import routers from controllers/
- *
- */
-const { templateRouter } = require('./controllers/template.js')
+
+const { fanRouter } = require('./controllers/fanController.js')
 
 
-/* Step 3
- *
- * Register middleware...
- */
 
-/* Step 3.a
- * ...to parse the body of the HTTP requests from a URL encoded string 
- */
 app.use(express.urlencoded({extended: true}))
 
 /* Step 3.b 
@@ -49,7 +29,7 @@ app.use(express.static(`${__dirname}/client/build`))
  * add router for the application to use. The first argument is a prefix to all
  * the paths defined in the router.
  */
-app.use('/api/helloworld', templateRouter)
+app.use('/api/fan', fanRouter)
 
 /* Step 5
  *
