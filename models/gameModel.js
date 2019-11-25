535340889c9106cdeb7596332c5ffef063935e47
Deivid Rodriguez <rodriguezdeivid17@gmail.com>
2019-11-25 14:16:45 -0500

    adding game model

--- models/gameModel.js
+++ models/gameModel.js
@@ -9,3 +9,34 @@ const GameSchema = new mongoose.Schema({
     country: String,
     zipcode: Number
 })
+
+const GameCollection = mongoose.model('Game', GameSchema)
+
+
+// get all 
+const getAllGames = () => {
+    return GameCollection.find({})
+}
+
+//get one
+const getSingleGame = (id) => {
+    return GameCollection.findById(id)
+}
+
+
+// create
+const createGame = (gameData) => {
+    return GameCollection.create(gameData)
+}
+
+//delete
+const deleteGame = (id) => {
+    return GameCollection.deleteOne({ _id: id })
+  }
+
+module.exports = {
+    getAllGames,
+    getSingleGame,
+    createGame,
+    deleteGame
+}
