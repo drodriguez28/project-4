e27fc66a3c45fc18aca5299b411437aff3dc18e3
Deivid Rodriguez <rodriguezdeivid17@gmail.com>
2019-11-26 11:07:54 -0500

    created jsx file for fans

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import HelloWorld from './components/HelloWorld.js'
 import './App.css';
+
 
 function App() {
   return (
---
+++ client/src/components/Fans.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import axios from "axios"
+import { Link } from 'react-router-dom'
+
+
+export default class Fans extends Component {
+    state = {
+        fans: []
+    }
+    componentDidMount = () => {
+        axios.get('/api/fan')
+            .then((response) => {
+                this.setState({ fans: response.data })
+            })
+    }
+    render() {
+        return (
+            <div>
+                <h1>Liga MX Fans</h1>
+                {this.state.fans.map((fan) => {
+                    return (
+                        <div key={fan._id}>
+                            <Link to={"/fans/" + fan._id}><h5>{fan.name}</h5></Link>
+
+                        </div>
+                    )
+                })}
+                {/* <footer><img src='./paintcan.jpeg' />
+
+                </footer> */}
+            </div>
+        )
+    }
+}
