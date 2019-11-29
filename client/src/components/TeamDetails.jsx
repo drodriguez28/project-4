9f5c3ff5c80cd947da0c6e191fe09726fcda9e67
Deivid Rodriguez <rodriguezdeivid17@gmail.com>
2019-11-29 10:24:33 -0500

    adding the delte to the team

--- client/src/components/TeamDetails.jsx
+++ client/src/components/TeamDetails.jsx
@@ -1,20 +1,57 @@
 import React, { Component } from 'react'
+import axios from "axios"
+
+
+
 
 export default class TeamDetails extends Component {
 
+    state = {
+        team: {}
+    }
+
+    deleteTeam = () => {
+        axios.delete('/api/team/' + this.props.match.params.id)
+            .then((response) => {
+                this.setState({ deleted: true })
+                console.log(response)
+            })
+    }
+
+
+
+    componentDidMount = () => {
+        axios.get('/api/team/' + this.props.match.params.id)
+            .then((reponse) => {
+                this.setState({ team: reponse.data })
+                console.log(this.state)
+            })
+    }
 
 
 
 
-
-
-
-    
     render() {
-        return (
-            <div>
-                
-            </div>
-        )
+        if (this.state.hasOwnProperty('deleted') && this.state.deleted) {
+            return (
+                <div>
+                    Team Deleted
+                </div>
+            )
+        } else {
+
+            return (
+                <div>
+                    <h1>Team Info</h1>
+                    <h5>name: {this.state.fan.name}</h5>
+
+
+
+
+
+
+                </div>
+            )
+        }
     }
 }
